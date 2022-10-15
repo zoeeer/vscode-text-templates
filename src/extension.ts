72b3d5f0a61bb41992c80870755a1a71faf654e7
zoeeer <rang.zoe1@gmail.com>
2022-10-16 00:25:50 +0800

    feat: file explorer
    reference: vscode-extension-samples/tree-view-sample

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { FileExplorer } from './fileExplorer';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -9,6 +10,8 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "text-templates" is now active!');
+
+	new FileExplorer(context);
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
